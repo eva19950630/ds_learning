0d78c89705a6cde1c7f968378d285b901dc18986
eva19950630 <fun910347@gmail.com>
2017-08-24 10:32:58 +0800

    index: right side page ok

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -8,19 +8,48 @@ $("body a").on("click", function(e){
 			document.getElementById("menusidepage").style.width = "350px";
 			break;
 		case "#learning":
-			document.getElementById("learningsidepage").style.width = "350px";
+			document.getElementById("learningsidepage").style.width = "345px";
 			break;
 		case "#note":
-			document.getElementById("notesidepage").style.width = "350px";
+			document.getElementById("notesidepage").style.width = "345px";
+			break;
+		case "#signup":
+			document.getElementById("signupsidepage").style.width = "350px";
+			break;
+		case "#login":
+			document.getElementById("loginsidepage").style.width = "350px";
+			break;
+		case "#":
+			closesidepage(idvar);
 			break;
 	}
 	switch (idvar) {
-		case "closeslide":
-			document.getElementById("menusidepage").style.width = "0";
-			break;
 		case "backslide":
 			document.getElementById("learningsidepage").style.width = "0";
 			document.getElementById("notesidepage").style.width = "0";
 			break;
+		case "changetologin":
+			document.getElementById("signupsidepage").style.width = "0";
+			document.getElementById("loginsidepage").style.width = "350px";
+			break;
+		case "changetosignup":
+			document.getElementById("loginsidepage").style.width = "0";
+			document.getElementById("signupsidepage").style.width = "350px";
+			break;
 	}
 });
+
+function closesidepage(idvar) {
+	// console.log(idvar);
+	switch (idvar) {
+		case "closemenu":
+			document.getElementById("menusidepage").style.width = "0";
+			break;
+		case "closesignup":
+			document.getElementById("signupsidepage").style.width = "0";
+			break;
+		case "closelogin":
+			document.getElementById("loginsidepage").style.width = "0";
+			break;
+	}
+}
