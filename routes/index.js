dcef36c076cd34d0f194d4bbd82ce3d9ce0f580e
eva19950630 <fun910347@gmail.com>
2017-09-04 00:42:06 +0800

    UI modify details

--- routes/index.js
+++ routes/index.js
@@ -113,7 +113,7 @@ module.exports = function(app) {
     });
   });
 
-  app.get('/hello', function (req, res) {
-    res.send('hello world!');
-  });
+  // app.get('/hello', function (req, res) {
+  //   res.send('hello world!');
+  // });
 };
